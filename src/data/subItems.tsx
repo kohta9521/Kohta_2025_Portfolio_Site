f14d4511092ea333832f4ffb035654ddb7b6783b
k_95.21 <kohta0421kochi@gmail.com>
2024-12-02 22:04:48 +0900

    add new headerlist item data

--- src/data/subItems.tsx
+++ src/data/subItems.tsx
@@ -2,21 +2,69 @@
 import { SubItem } from "@/types/types";
 
 // react icons
-import { FaUser, FaGlobe } from "react-icons/fa";
+import { FaUser, FaRocket } from "react-icons/fa";
+import { RiProfileLine } from "react-icons/ri";
 
 export const aboutSubItems: SubItem[] = [
   {
     key: "profile",
     link: "/about/#profile",
     reactIcon: <FaUser />,
-    jaTitle: "プロフィール",
-    enTitle: "Profile",
+    jaTitle: "自己紹介",
+    enTitle: "My Profile",
   },
   {
-    key: "whatthissite",
-    link: "/about/#whatthissite",
-    reactIcon: <FaGlobe />,
-    jaTitle: "このサイトについて",
-    enTitle: "About This Site",
+    key: "career",
+    link: "/about/#career",
+    reactIcon: <RiProfileLine />,
+    jaTitle: "これまでとこれからのキャリア",
+    enTitle: "Career",
+  },
+  {
+    key: "hobby",
+    link: "/about/#hobby",
+    reactIcon: <FaRocket />,
+    jaTitle: "趣味や興味",
+    enTitle: "Hobby",
+  },
+];
+
+export const profileSubItems: SubItem[] = [
+  {
+    key: "profile",
+    link: "/about/#profile",
+    reactIcon: <FaUser />,
+    jaTitle: "自己紹介",
+    enTitle: "My Profile",
+  },
+];
+
+export const worksSubItems: SubItem[] = [
+  {
+    key: "works",
+    link: "/works/#works",
+    reactIcon: <FaUser />,
+    jaTitle: "制作物",
+    enTitle: "Works",
+  },
+];
+
+export const blogSubItems: SubItem[] = [
+  {
+    key: "blog",
+    link: "/blog/#blog",
+    reactIcon: <FaUser />,
+    jaTitle: "ブログ",
+    enTitle: "Blog",
+  },
+];
+
+export const contactSubItems: SubItem[] = [
+  {
+    key: "contact",
+    link: "/contact/#contact",
+    reactIcon: <FaUser />,
+    jaTitle: "お問い合わせ",
+    enTitle: "Contact",
   },
 ];
